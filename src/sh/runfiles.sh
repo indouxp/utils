daeee3d3abcb18e2f657a51596432fda81c6d69e
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-09-09 17:41:44 +0900

    update runfiles.sh

--- src/sh/runfiles.sh
+++ src/sh/runfiles.sh
@@ -19,7 +19,7 @@ trap 'term' 0
 
 main() {
   FILE=`basename $1`
-  awk -vlogfile=/tmp/${SELF:?}.${FILE:?}.log '
+  awk '
     function hline(n) {
       for (i = 0; i < n; i++) {
         printf("#");
@@ -27,20 +27,18 @@ main() {
       printf("\n");
     }
     BEGIN{
-      time = sprintf("`date %c+%%Y%%m%%d.%%H%%M%%S%c`", 0x27, 0x27);
+      time = sprintf("`date %c+%%Y%%m%%d.%%H%%M%%S%c`", 39, 39);
       printf("#!/bin/sh\n");
-      printf("rm -f %s\n", logfile);
     }
     {
       hline(79);
       printf("echo \"%s\"\n", $0);
       printf("%s\n", $0);
       printf("if [ \"$?\" -ne \"0\" ]; then\n");
-      printf("  echo \"%s:%s fail.\" >> %s\n", time, $0, logfile);
-      printf("  echo \"%s:%s fail.\" 1>&2\n", time, $0);
+      printf("  echo \"%s:%s:fail.\" 1>&2\n", time, $0);
       printf("  exit 1\n");
       printf("fi\n");
-      printf("echo \"%s:%s done\" >> %s\n", time, $0, logfile);
+      printf("echo \"%s:%s:done.\"\n", time, $0, logfile);
     }
     END{
       printf("exit 0\n");
@@ -51,8 +49,9 @@ main() {
     exit 1
   fi
 
-  chmod a+x /tmp/${SELF:?}.${FILE:?}.sh
-  /tmp/${SELF:?}.${FILE:?}.sh
+  chmod u+x /tmp/${SELF:?}.${FILE:?}.sh
+  cat /tmp/${SELF:?}.${FILE:?}.sh >   /tmp/${SELF:?}.${FILE:?}.log
+  /tmp/${SELF:?}.${FILE:?}.sh     >>  /tmp/${SELF:?}.${FILE:?}.log 2>&1
   return $?
 }
 
