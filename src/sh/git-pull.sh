10a66435e6bfb80da9447138ac835a5d41038f82
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-04-27 11:56:41 +0900

    update git-pull.sh

--- src/sh/git-pull.sh
+++ src/sh/git-pull.sh
@@ -2,6 +2,8 @@
 
 for DIR in utils work/samples work/scraping
 do
-  (cd $HOME/$DIR && git pull)
-  echo "`basename $0`:$HOME/$DIR done"
+  if [ -d $HOME/$DIR ]; then
+    (cd $HOME/$DIR && git pull)
+    echo "`basename $0`:$HOME/$DIR done"
+  fi
 done
