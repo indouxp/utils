6e80c99de321bcdf9cb40758791ea739c194621d
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-04-30 22:14:02 +0900

    update git-pull.sh

--- src/sh/git-pull.sh
+++ src/sh/git-pull.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-for DIR in utils work/samples work/scraping
+for DIR in              \
+          utils         \
+          work/samples  \
+          work/scraping \
+          work/design-pattern
 do
   if [ -d $HOME/$DIR ]; then
     (cd $HOME/$DIR && git pull)
