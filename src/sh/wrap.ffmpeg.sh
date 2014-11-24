f85bb7ac7b979ff78335f34ca385cb2c960a3605
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-11-24 16:41:18 +0900

    update wrap.ffmpeg.sh

--- src/sh/wrap.ffmpeg.sh
+++ src/sh/wrap.ffmpeg.sh
@@ -16,6 +16,7 @@ do
   if [ "${count:?}" -eq "0" ]; then
     echo "do ${Prefix:?}"
     ffmpeg -i $file -f image2 -vf fps=1 ${Prefix:?}.%05d.jpg
+    mv ${Prefix:?}.*.jpg /media/disk/PHTO
   else
     echo "skip ${Prefix:?}"
   fi 
