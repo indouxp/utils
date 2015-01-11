68fa463a4b325e30a235c23a20f70f79fc14871e
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2015-01-11 10:12:56 +0900

    add mail-log.sh

---
+++ src/sh/mail-log.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# /var/log以下のログをメール
+#
+#
+NAME=`basename $0`
+SUBJECT="`hostname`:`uname -s`:${NAME:?}:`date '+%Y%m%d.%H%M%S'`"
+MAILTO="tatsuo-i@mtb.biglobe.ne.jp"
+
+for file in           \
+  /var/log/messages   \
+  /var/log/syslog     \
+  /var/log/daemon.log \
+  /var/log/kern.log   \
+  /var/log/dpkg.log   \
+  /var/log/auth.log
+do
+  echo ${file:?}
+  sed "s/^/  /" ${file:?}
+done | mail -s "${SUBJECT}" ${MAILTO:?}
+
