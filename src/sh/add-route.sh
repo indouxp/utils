922802fa261dd5910ad79ca89ab2d4af1cd7ec15
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2015-04-29 23:00:40 +0900

    add add-route.sh

---
+++ src/sh/add-route.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+SCRIPT_NAME=`basename $0`
+
+# wzr-hp-g450hに振られている可能性のあるアドレスを順に処理
+for wzr_hp_g450h in \
+  192.168.0.2\
+  192.168.0.3\
+  192.168.0.4\
+  192.168.0.5\
+  192.168.0.6
+do
+  if ping -c1 $wzr_hp_g450h >/dev/null 2>&1; then
+    # routeに192.168.11.0ネットワークを登録し、192.168.11.1へのpingを見る
+    # wzr-hp-g450hには、192.168.11.1がLAN側のIP ADDRESSとして振られている
+    route add -net 192.168.11.0 netmask 255.255.255.0 gw $wzr_hp_g450h
+    if ping -c1 192.168.11.1 >/dev/null 2>&1; then
+      echo "OK" $wzr_hp_g450h
+      logger "${SCRIPT_NAME:?}: routing done."
+      date  >   /var/log/${SCRIPT_NAME:?}.log
+      route >>  /var/log/${SCRIPT_NAME:?}.log
+      # pingが通ればOK
+      break
+    else
+      echo "NG" $wzr_hp_g450h
+      logger "${SCRIPT_NAME:?}:${wzr_hp_g450h:?}:ng."
+    fi
+    route del -net 192.168.11.0 netmask 255.255.255.0
+  else
+    echo "NG" $wzr_hp_g450h
+    logger "${SCRIPT_NAME:?}: $wzr_hp_g450h not found."
+    exit 1
+  fi
+done
+exit 0
