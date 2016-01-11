983d62b11313be74653d3b0a93738242b9e1cc21
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2016-01-12 02:13:58 +0900

    add chk-ntpq.sh

---
+++ src/sh/chk-ntpq.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+SCRIPT=${0##*/}
+TMPFILE=/tmp/${SCRIPT:?}.$$
+###############################################################################
+#
+# raspberrypi の動作確認用
+#
+###############################################################################
+term() {
+  rm -f /tmp/${0##*}.$$
+}
+
+trap 'term' 0 1 2 3 15
+
+# ntpq確認
+result=$(ntpq -p | tee $TMPFILE | /bin/grep "^\*")
+[ -z "$result" ] && logger "${SCRIPT:?}:ntpq fail"
+logger "${SCRIPT:?}:ntpq ok"
