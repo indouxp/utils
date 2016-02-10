aa572c9ac7ad49a6a589dd4673bdb4294a895ef9
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2016-02-10 18:24:37 +0900

    update shdw-host-by-sw.sh:chk-ntpq.shで時刻同期後、開始するように変更

--- src/sh/shdw-host-by-sw.sh
+++ src/sh/shdw-host-by-sw.sh
@@ -7,6 +7,14 @@ LOG=/var/log/${0##*/}.log
 # GPIO_7を(Pin26)使用
 GPIO_NO=7
 
+if [ -x /home/pi/utils/src/sh/chk-ntpq.sh ]; then
+  rc=""
+  while [ $rc -ne 0 ]; do
+    /home/pi/utils/src/sh/chk-ntpq.sh # ntpにより、時刻が正しいか
+    rc=$?
+  done
+fi
+
 echo "`date '+%Y%m%d.%H%M%S'`:${0##*/}:start"   >> ${LOG:?}
 echo ${GPIO_NO:?} > /sys/class/gpio/export
 
@@ -29,5 +37,6 @@ done
 
 echo ${GPIO_NO:?} > /sys/class/gpio/unexport
 
+/home/pi/utils/src/sh/shdw-raspi.sh               >> ${LOG:?}
 echo "`date '+%Y%m%d.%H%M%S'`:${0##*/}:shutdown"  >> ${LOG:?}
 shutdown -h now
