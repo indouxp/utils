bfe5c817623355997536aaa883637749607938d9
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2016-02-18 06:14:42 +0900

    update chk-ntpq.sh

--- src/sh/chk-ntpq.sh
+++ src/sh/chk-ntpq.sh
@@ -14,6 +14,11 @@ trap 'term' 0 1 2 3 15
 
 # ntpq確認
 result=$(ntpq -p | tee $TMPFILE | /bin/grep "^\*")
-[ -z "$result" ] && logger -i "${SCRIPT:?}:ntpq fail" && exit 1
+
+[ -z "$result" ] &&\
+  cp $TMPFILE /var/log/chk-ntpq.sh.`date '+%d'` &&\
+  logger -i "${SCRIPT:?}:ntpq fail" &&\
+  exit 1
+
 logger -i "${SCRIPT:?}:ntpq ok"
 exit 0
