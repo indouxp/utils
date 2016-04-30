2970851fabd365e41f8a8c5f93027b6b0fce2e13
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2016-04-30 18:50:57 +0900

    update .PS1.bash

--- dotfile/.PS1.bash
+++ dotfile/.PS1.bash
@@ -1,5 +1,5 @@
-
-if id | grep "=0[^0-9]" > /dev/null; then
+# http://d.hatena.ne.jp/zariganitosh/20150224/escape_sequence
+if id | grep "=0[^0-9]" > /dev/null; then # uid=0
   #PS1="\$? \[\e[36m\e[41m\]\D{%y%m%d-%H%M%S} \u@\h:\W:\$\[\e[0m\] "
   PS1="\$? \[\e[31m\e[40m\]\D{%y%m%d-%H%M%S} \u@\h:\W:\$\[\e[0m\] "
 else
