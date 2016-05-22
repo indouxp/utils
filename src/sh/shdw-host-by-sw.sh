1db0ab2849a53aab453c28eedebd2399c5ad1187
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2016-05-22 14:22:50 +0900

    update shdw-host-by-sw.sh

--- src/sh/shdw-host-by-sw.sh
+++ src/sh/shdw-host-by-sw.sh
@@ -9,13 +9,16 @@ GPIO_NO=7
 
 if [ -x /home/pi/utils/src/sh/chk-ntpq.sh ]; then
   rc=""
-  while [ $rc -ne 0 ]; do
+  while [ "$rc" != "0" ]; do
     /home/pi/utils/src/sh/chk-ntpq.sh # ntpにより、時刻が正しいか
     rc=$?
+    sleep 1 
   done
 fi
 
-echo "`date '+%Y%m%d.%H%M%S'`:${0##*/}:start"   >> ${LOG:?}
+msg="`date '+%Y%m%d.%H%M%S'`:${0##*/}:start"
+echo ${msg:?} | tee -a ${LOG:?} | logger
+
 echo ${GPIO_NO:?} > /sys/class/gpio/export
 
 while [ ! -f /sys/class/gpio/gpio${GPIO_NO:?}/direction ]
@@ -37,6 +40,13 @@ done
 
 echo ${GPIO_NO:?} > /sys/class/gpio/unexport
 
-/home/pi/utils/src/sh/shdw-raspi.sh               >> ${LOG:?}
-echo "`date '+%Y%m%d.%H%M%S'`:${0##*/}:shutdown"  >> ${LOG:?}
+#/home/pi/utils/src/sh/shdw-raspi.sh               >> ${LOG:?} 2>&1
+HOSTS="192.168.11.170 192.168.11.171 192.168.0.80 192.168.0.81"
+for HOST in $HOSTS
+do
+  su pi -c "ssh pi@$HOST 'sudo shutdown -h now' &"  >> ${LOG:?} 2>&1
+done
+
+msg="`date '+%Y%m%d.%H%M%S'`:${0##*/}:shutdown"
+echo ${msg:?} | tee -a ${LOG:?} | logger
 shutdown -h now
