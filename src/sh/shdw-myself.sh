22cf8389c7614eab416e39e436839084388edf46
Tatsuo Indou <indou.tsystem@gmail.com>
2016-09-06 06:42:07 +0900

    update shdw-myself.sh

--- src/sh/shdw-myself.sh
+++ src/sh/shdw-myself.sh
@@ -4,17 +4,25 @@
 #
 ###############################################################################
 LOG=/var/log/${0##*/}.log
-WAIT_SEC=30
 GW=192.168.0.254
 
 msg="`date '+%Y%m%d.%H%M%S'`:${0##*/}:start"
 echo ${msg:?} | tee ${LOG:?} | logger
+
+COUNT=0
 while true
 do
   if ! ping -c1 ${GW:?} >> ${LOG:?}; then
-    break
+    COUNT=`expr ${COUNT:?} + 1`
+    echo $COUNT
+    if [ ${COUNT:?} -eq 5 ]; then
+      break
+    fi
+    sleep 1
+  else
+    sleep 30
   fi
-  sleep ${WAIT_SEC:?}
 done
+
 msg="`date '+%Y%m%d.%H%M%S'`:${0##*/}:shutdown"
 echo ${msg:?} | tee -a ${LOG:?} | logger
