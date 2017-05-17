355d96cb329d2ddfe912dd47206c4d23fee4637d
Tatsuo Indou <indou.tsystem@gmail.com>
2017-05-17 21:37:03 +0900

    update chk-login.sh

--- src/sh/chk-login.sh
+++ src/sh/chk-login.sh
@@ -16,51 +16,53 @@ tolog() {
 }
 main() {
 
-	tolog "START"
+  tolog "START"
 
-	# last
-	# -a:ホスト名を最後の欄に表示する。
-	# -d:IP アドレスをホスト名に変換し直して表示する。
-	last -a -d |	
-	awk '
-		BEGIN{
-		}
-		{
-			if ($1 == "reboot") {
-				next;
-			}
-			if ($NF ~ /192\.168\./) {
-				next;
-			}
-			if ($NF ~ /tsystem\.gr\.jp/) {
-				next;
-			}
-			if ($NF ~ /0\.0\.0\.0/) {
-				next;
-			}
-			print;
-		}
-		END{
-		}' > ${NOW:?}
-	RC=$?
-	[ ${RC:?} -ne 0 ] && msg="${SCRIPT:?}:fail. at 010" && logger $msg && tolog $msg && exit 9
+  # last
+  # -a:ホスト名を最後の欄に表示する。
+  # -d:IP アドレスをホスト名に変換し直して表示する。
+  # -n:n行表示
+  last -a -d -n 10 |
+  awk '
+    BEGIN{
+    }
+    {
+      if ($1 == "reboot") {
+        next;
+      }
+      if ($NF ~ /192\.168\./) {
+        next;
+      }
+      if ($NF ~ /tsystem\.gr\.jp/) {
+        next;
+      }
+      if ($NF ~ /0\.0\.0\.0/) {
+        next;
+      }
+      print;
+    }
+    END{
+    }' > ${NOW:?}
+  RC=$?
+  [ ${RC:?} -ne 0 ] && msg="${SCRIPT:?}:fail. at 010" && logger $msg && tolog $msg && exit 9
 
-	touch ${MASTER:?}
-	diff ${MASTER:?} ${NOW:?} | head -2 | tail -1 > ${TMP:?}
-	RC=$?
-	[ ${RC:?} -ne 0 ] && msg="${SCRIPT:?}:fail. at 020" && logger $msg && tolog $msg && exit 9
+  touch ${MASTER:?}
+  #diff ${MASTER:?} ${NOW:?} | head -2 | tail -1 > ${TMP:?}
+  diff ${MASTER:?} ${NOW:?} | grep ">" > ${TMP:?}
+  RC=$?
+  [ ${RC:?} -ne 0 ] && msg="${SCRIPT:?}:fail. at 020" && logger $msg && tolog $msg && exit 9
 
-	if [ -s ${TMP:?} ]; then
+  if [ -s ${TMP:?} ]; then
     cat ${TMP:?} >> ${LOG:?}
-		cat ${TMP:?} | mail -s "Invasion information" ${MAIL:?}
-		[ ${RC:?} -ne 0 ] && msg="${SCRIPT:?}:fail. at 030" && logger $msg && tolog $msg && exit 9
+    cat ${TMP:?} | mail -s "Invasion information" ${MAIL:?}
+    [ ${RC:?} -ne 0 ] && msg="${SCRIPT:?}:fail. at 030" && logger $msg && tolog $msg && exit 9
     for i in `awk 'BEGIN{for (i = 0; i < 5; i++) {print i;}}'`
     do
       mpg321 /home/pi/data/se_maoudamashii_chime01.mp3 &
     done
-	fi
+  fi
 
-	mv ${NOW:?} ${MASTER:?}
+  mv ${NOW:?} ${MASTER:?}
 
   tolog "DONE"
 }
