765f751a00011996d47d8460b6180b64fd57e100
Tatsuo Indou <indou.tsystem@gmail.com>
2017-08-16 09:35:08 +0900

    add alarm-3wed.sh

---
+++ src/sh/alarm-3wed.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+###############################################################################
+#
+# 第三水曜日の場合
+#
+###############################################################################
+TMP=/tmp/${0##*/}.tmp.$$
+
+TERM() {
+  rm -f /tmp/${0##*/}.tmp.$$
+}
+
+trap 'TERM' 0
+
+LANG=C cal > $TMP
+
+TODAY=`date '+%d'`
+
+sed -n '/Su Mo Tu We Th Fr Sa/,$p' $TMP |
+awk -v today=$TODAY '
+  {
+    if (NR == 4) {              # title行より3行目
+      if (length($4) == 6) {    # 第三水曜日は4フィールド目で、制御文字を含むため文字列長が6の場合 
+        wed3 = sprintf("%s%s", substr($4, 3, 1), substr($4, 6, 1));
+        if (wed3 == today) {      
+          exit 9;
+        }
+      }
+    }
+  }
+'
+[ $? = 9 ] && mpg321 /home/pi/data/se_maoudamashii_chime01.mp3 &
