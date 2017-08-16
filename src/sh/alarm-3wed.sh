abef0efb5736a620c65c694038d38bc3b757e651
Tatsuo Indou <indou.tsystem@gmail.com>
2017-08-16 09:52:51 +0900

    update  alarm-3wed.sh

--- src/sh/alarm-3wed.sh
+++ src/sh/alarm-3wed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ###############################################################################
 #
-# 第三水曜日の場合
+# crontabで、0 8 * * * CMD に実行され、第三水曜日の場合きゃーという
 #
 ###############################################################################
 TMP=/tmp/${0##*/}.tmp.$$
@@ -20,7 +20,7 @@ sed -n '/Su Mo Tu We Th Fr Sa/,$p' $TMP |
 awk -v today=$TODAY '
   {
     if (NR == 4) {              # title行より3行目
-      if (length($4) == 6) {    # 第三水曜日は4フィールド目で、制御文字を含むため文字列長が6の場合 
+      if (length($4) == 6) {    # 第三水曜日は4フィールド目で、当日の場合制御文字を含むため文字列長が6
         wed3 = sprintf("%s%s", substr($4, 3, 1), substr($4, 6, 1));
         if (wed3 == today) {      
           exit 9;
@@ -29,4 +29,7 @@ awk -v today=$TODAY '
     }
   }
 '
-[ $? = 9 ] && mpg321 /home/pi/data/se_maoudamashii_voice_human02.mp3 &
+RC=$?
+[ $RC = 9 ] && mpg321 /home/pi/data/se_maoudamashii_voice_human02.mp3
+[ $RC = 9 ] && mpg321 /home/pi/data/se_maoudamashii_voice_human02.mp3
+[ $RC = 9 ] && mpg321 /home/pi/data/se_maoudamashii_voice_human02.mp3 &
