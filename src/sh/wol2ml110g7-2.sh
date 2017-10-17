2383e0f18ffc770afe92cf401350cbf3ea35a46d
Tatsuo Indou <indou.tsystem@gmail.com>
2017-10-17 23:10:36 +0900

    update wol2ml* wol2stormZ170.sh  wol2t3600.sh wol2vip.sh

--- src/sh/wol2ml110g7-2.sh
+++ src/sh/wol2ml110g7-2.sh
@@ -8,4 +8,7 @@ echo "wol:${RC:?}"
 [ -x /usr/sbin/ufw ] && sudo ufw -f enable
 [ -x /usr/sbin/ufw ] && sudo ufw status
 
-
+NAME=${0##*/}
+LOG=/var/log/${0##*/}.log
+sudo touch ${LOG:?} && sudo chmod a+w ${LOG:?}
+echo "${NAME:?} done. `date '+%Y%m%dT%H%M%S'`" >> ${LOG:?}
