cd4be919ad04dfcc5bbd0d109997db7e4a75ae00
Tatsuo Indou <indou.tsystem@gmail.com>
2017-11-19 01:54:57 +0900

    add alarm-WorkingDay.sh

---
+++ src/sh/alarm-WorkingDay.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export LANG=C
+
+[ `date +%A` = "Sunday" ] && exit 0
+#[ `date +%A` = "Monday" ] && exit 0
+#[ `date +%A` = "Tuesday" ] && exit 0
+[ `date +%A` = "Wednesday" ] && exit 0
+#[ `date +%A` = "Thursday" ] && exit 0
+#[ `date +%A` = "Friday" ] && exit 0
+[ `date +%A` = "Saturday" ] && exit 0
+
+for FILE in `ls -1 /home/pi/data/*.ogg`
+do
+  ogg123 $FILE
+done
+
+exit 0
