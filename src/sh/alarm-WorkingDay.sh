47d9855cda7fe45915de564fe17db41bdfe94627
Tatsuo Indou <indou.tsystem@gmail.com>
2017-11-23 20:00:43 +0900

    update alarm-WorkingDay.sh

--- src/sh/alarm-WorkingDay.sh
+++ src/sh/alarm-WorkingDay.sh
@@ -4,19 +4,21 @@
 #
 #
 ###############################################################################
+LOG=/home/pi/${0##*/}.log
 export LANG=C
 
-[ `date +%A` = "Sunday" ] && exit 0
-#[ `date +%A` = "Monday" ] && exit 0
-#[ `date +%A` = "Tuesday" ] && exit 0
-[ `date +%A` = "Wednesday" ] && exit 0
-#[ `date +%A` = "Thursday" ] && exit 0
-#[ `date +%A` = "Friday" ] && exit 0
-[ `date +%A` = "Saturday" ] && exit 0
+TODAY=`date +%A`
+[ ${TODAY:?} = "Sunday" ] && exit 0
+#[ ${TODAY:?} = "Monday" ] && exit 0
+#[ ${TODAY:?} = "Tuesday" ] && exit 0
+[ ${TODAY:?} = "Wednesday" ] && exit 0
+#[ ${TODAY:?} = "Thursday" ] && exit 0
+#[ ${TODAY:?} = "Friday" ] && exit 0
+[ ${TODAY:?} = "Saturday" ] && exit 0
 
 for FILE in `ls -1 /home/pi/data/*.ogg`
 do
-  ogg123 $FILE
+  ogg123 $FILE >>${LOG:?}
 done
 
 exit 0
