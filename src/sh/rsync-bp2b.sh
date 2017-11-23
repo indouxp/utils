3f4d3bfbf496c9cffdef3970f71c4f100a16c050
Tatsuo Indou <indou.tsystem@gmail.com>
2017-11-23 19:52:07 +0900

    update rsync-bp2b.sh

--- src/sh/rsync-bp2b.sh
+++ src/sh/rsync-bp2b.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-rsync -r -e "/usr/bin/ssh -i /home/pi/.ssh/id_rsa4rpi-bp" \
+rsync -a -e "/usr/bin/ssh -i /home/pi/.ssh/id_rsa4rpi-bp" \
   pi@rpi-bp:/home/pi/backup-rpi-bp \
   /home/pi/backup
 RC=$?
-exit ${RC:?}
+[ ${RC:?} != 0 ] && exit ${RC:?}
+
+rsync -a -e "/usr/bin/ssh -i /home/pi/.ssh/id_rsa4rpi-bp" \
+  pi@rpi-bp:/mnt/usb8G/important-documents \
+  /home/pi/backup
+RC=$?
+[ ${RC:?} != 0 ] && exit ${RC:?}
