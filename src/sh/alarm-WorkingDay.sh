e27ea13496aa8d7db8cbe3e37fbc47255700f6bd
Tatsuo Indou <indou.tsystem@gmail.com>
2017-11-27 11:28:08 +0900

    update alarm-WorkingDay.sh

--- src/sh/alarm-WorkingDay.sh
+++ src/sh/alarm-WorkingDay.sh
@@ -4,21 +4,25 @@
 #
 #
 ###############################################################################
-LOG=/home/pi/${0##*/}.log
+LOG=/home/pi/log/${0##*/}.log
 export LANG=C
 
+# 仕事の日以外は終了する
 TODAY=`date +%A`
-[ ${TODAY:?} = "Sunday" ] && exit 0
-#[ ${TODAY:?} = "Monday" ] && exit 0
-#[ ${TODAY:?} = "Tuesday" ] && exit 0
-[ ${TODAY:?} = "Wednesday" ] && exit 0
-#[ ${TODAY:?} = "Thursday" ] && exit 0
-#[ ${TODAY:?} = "Friday" ] && exit 0
-[ ${TODAY:?} = "Saturday" ] && exit 0
+[ ${TODAY:?}  = "Sunday"    ] && exit 0
+#[ ${TODAY:?} = "Monday"    ] && exit 0
+#[ ${TODAY:?} = "Tuesday"   ] && exit 0
+[ ${TODAY:?}  = "Wednesday" ] && exit 0
+#[ ${TODAY:?} = "Thursday"  ] && exit 0
+#[ ${TODAY:?} = "Friday"    ] && exit 0
+[ ${TODAY:?}  = "Saturday"  ] && exit 0
 
+# ~/data以下のoggの実行
 for FILE in `ls -1 /home/pi/data/*.ogg`
 do
-  ogg123 $FILE >>${LOG:?}
+  echo $FILE                >> ${LOG:?}
+  sudo /usr/bin/aumix -v 50 >> ${LOG:?} 2>&1
+  /usr/bin/ogg123 $FILE     >> ${LOG:?}
 done
 
 exit 0
