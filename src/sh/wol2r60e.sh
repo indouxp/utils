802f0336ffa3fa5858049989e922aa96ed7fd770
Tatsuo Indou <indou.tsystem@gmail.com>
2018-01-08 13:45:59 +0900

    add wol2r60e.sh

---
+++ src/sh/wol2r60e.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ -x /usr/sbin/ufw ] && sudo ufw disable
+wakeonlan 00:16:d3:23:ed:02 # r60e
+RC=$?
+echo "wol:${RC:?}"
+[ -x /usr/sbin/ufw ] && sudo ufw -f enable
+[ -x /usr/sbin/ufw ] && sudo ufw status
+
+NAME=${0##*/}
+LOG=/var/log/${0##*/}.log
+sudo touch ${LOG:?} && sudo chmod a+w ${LOG:?}
+echo "${NAME:?} done. `date '+%Y%m%dT%H%M%S'`" >> ${LOG:?}
