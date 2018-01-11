b2980c4c588c1433fe674dc1a4c9fc6cce8c045a
Tatsuo Indou <indou.tsystem@gmail.com>
2018-01-11 13:07:37 +0900

    update ./chk-all.sh

--- src/sh/chk-all.sh
+++ src/sh/chk-all.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-NAMES="ml110g7 ml110g7-2 t3600 vip rpi-bp rpi2 rpi-b macbook macmini2010 rpi3-1 rpi3-2"
-=======
-NAMES="ml110g7 ml110g7-2 t3600 vip rpi-bp rpi2 rpi-b macbook stormZ170 r60e"
->>>>>>> de455610509be9b0dcf1ee73af4bb546a8fcf3e6
+NAMES="ml110g7 ml110g7-2 t3600 vip rpi-bp rpi2 rpi-b macbook macmini2010 rpi3-1 rpi3-2 stormZ170 r60e w7ee"
 
 for NAME in ${NAMES:?}
 do
