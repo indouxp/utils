3e6a26090f895de1c78727527ebcbb629ccb0249
Tatsuo Indou <indou.tsystem@gmail.com>
2018-01-28 13:08:56 +0900

    update chk-avi.sh

--- src/sh/chk-avi.sh
+++ src/sh/chk-avi.sh
@@ -15,10 +15,10 @@ do
   NOW_AVI=$(ls ${TARGET_DIR:?}/*.avi | wc -l)
   NOW=`date '+%m月%d日 %H時%M分%S秒'`
   if [ "${ORG_AVI:?}" -lt "${NOW_AVI:?}" ]; then
-    echo "動体を検知しました。${ORG_AVI:?} -> ${NOW_AVI:?}"       > ${NOTICE:?}
-    echo "動体を検知しました。${ORG_AVI:?} -> ${NOW_AVI:?} $NOW" >> ${LOG:?}
-  #else
-  #  echo "変化はありません。$NOW"   > ${NOTICE:?}
+    echo "動体を検知しました。動体検知ファイルが、${NOW_AVI:?}ファイルあります。" >  ${NOTICE:?}
+    echo "動体を検知しました。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"              >> ${LOG:?}
+  else
+    echo "変化はありません。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"                >> ${LOG:?}
   fi
   sleep ${SLEEP_SEC:?}
   ORG_AVI=${NOW_AVI:?}
