f21ac39bb6c8059003dab891ff37104aacd31413
Tatsuo Indou <indou.tsystem@gmail.com>
2018-02-01 23:27:49 +0900

    update talk.sh

--- src/sh/talk.sh
+++ src/sh/talk.sh
@@ -4,6 +4,26 @@
 # REFERENCE:https://qiita.com/lutecia16v/items/8d220885082e40ace252
 #
 ###############################################################################
+NAME=${0##*/}
+IS_LOCK=0
+LOCK_DIR=/tmp/${NAME:?}.lock.d
+
+term() {
+  if [ ${IS_LOCK:?} != 0 ]; then
+    rmdir /tmp/${NAME:?}.lock.d
+  fi
+}
+trap 'term' 0 1 2 3 15
+
+while true
+do
+  if mkdir ${LOCK_DIR:?} 2>/dev/null; then
+    IS_LOCK=1
+    break
+  fi
+  sleep 1
+done
+
 VOICE=/tmp/${0##*/}.$$.voice
 WAV=/tmp/${0##*/}.$$.wav
 
