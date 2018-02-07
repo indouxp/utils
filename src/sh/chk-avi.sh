998f577ddee869e493c5413b62a5546f5410751a
Tatsuo Indou <indou.tsystem@gmail.com>
2018-02-08 00:23:56 +0900

    update chk-avi.sh

--- src/sh/chk-avi.sh
+++ src/sh/chk-avi.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+###############################################################################
+#
+# motionの出力するaviファイルの数を数えて、前回との差がある場合、talk.shを呼び出す
+# nohup でバックグラウンド実行するか、/etc/rc.localからnohup でバックグラウンド実行させる。
+#
+###############################################################################
 NAME=${0##*/}
 
 LOG_DIR=/var/log
@@ -17,9 +23,12 @@ do
   NOW=`date '+%m月%d日 %H時%M分%S秒'`
   if [ "${ORG_AVI:?}" -lt "${NOW_AVI:?}" ]; then
     echo "動体を検知しました。AVIファイルが、${ORG_AVI:?}ファイルから、${NOW_AVI:?}ファイルに増加しました。" >  ${NOTICE:?}
-    echo "動体を検知しました。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"              >> ${LOG:?}
-  else
-    echo "変化はありません。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"                >> ${LOG:?}
+    cat <<EOT | tail ${NOTICE:?} | mail -s "${NAME:?}" indou.tsystem@gmail.com,toshikoyumechan@ezweb.ne.jp
+動体を検知しました。AVIファイルが、${ORG_AVI:?}ファイルから、${NOW_AVI:?}ファイルに増加しました。"
+EOT
+#    echo "動体を検知しました。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"              >> ${LOG:?}
+#  else
+#    echo "変化はありません。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"                >> ${LOG:?}
   fi
   sleep ${SLEEP_SEC:?}
   ORG_AVI=${NOW_AVI:?}
