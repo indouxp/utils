5e4a573c80c3441765b193a9cfbaba2bced1b767
Tatsuo Indou <indou.tsystem@gmail.com>
2018-02-12 12:13:39 +0900

    update webcam2rpi-b.sh

--- src/sh/chk-avi.sh
+++ src/sh/chk-avi.sh
@@ -6,6 +6,8 @@
 #
 ###############################################################################
 NAME=${0##*/}
+UTF8=/tmp/${NAME:?}.utf8
+SJIS=/tmp/${NAME:?}.cp932
 
 LOG_DIR=/var/log
 LOG_NAME=${NAME:?}.log
@@ -17,15 +19,22 @@ TARGET_DIR=/mnt/usb8G/webcam
 SLEEP_SEC=5
 
 ORG_AVI=$(ls ${TARGET_DIR:?}/*.avi | wc -l)
+LAST=`ls -1 ${TARGET_DIR:?}/*.avi | tail -1 | sed "s%${TARGET_DIR:?}%/webcam%"`
 while true
 do
   NOW_AVI=$(ls ${TARGET_DIR:?}/*.avi | wc -l)
   NOW=`date '+%m月%d日 %H時%M分%S秒'`
   if [ "${ORG_AVI:?}" -lt "${NOW_AVI:?}" ]; then
-    echo "動体を検知しました。AVIファイルが、${ORG_AVI:?}ファイルから、${NOW_AVI:?}ファイルに増加しました。" >  ${NOTICE:?}
-    cat <<EOT | tail ${NOTICE:?} | mail -s "${NAME:?}" indou.tsystem@gmail.com,toshikoyumechan@ezweb.ne.jp
+    cat <<EOT | tee ${UTF8:?} | nkf -s > ${SJIS:?}
 動体を検知しました。AVIファイルが、${ORG_AVI:?}ファイルから、${NOW_AVI:?}ファイルに増加しました。"
 EOT
+    cat ${UTF8:?}   > ${NOTICE:?}
+    cat <<EOT | tee -a ${UTF8:?} | nkf -s >> ${SJIS:?}
+    http://192.168.0.254/${LAST:?}
+EOT
+    cat ${UTF8:?}   | mail -s "${NAME:?}" indou.tsystem@gmail.com,toshikoyumechan@ezweb.ne.jp
+    cat ${SJIS:?}   | mail -s "${NAME:?}" indou.tsystem@docomo.ne.jp
+
 #    echo "動体を検知しました。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"              >> ${LOG:?}
 #  else
 #    echo "変化はありません。${ORG_AVI:?} -> ${NOW_AVI:?} ${NOW:?}"                >> ${LOG:?}
