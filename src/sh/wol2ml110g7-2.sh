ff6bc39ca1958772dfd0bd2343c1109726b1cebc
Tatsuo Indou <indou.tsystem@gmail.com>
2018-04-05 11:58:12 +0900

    update wol2ml110g7-2.sh

--- src/sh/wol2ml110g7-2.sh
+++ src/sh/wol2ml110g7-2.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 [ -x /usr/sbin/ufw ] && sudo ufw disable
-#wakeonlan e8:39:35:2a:8a:08 # ml110g7
-wakeonlan e4:11:5b:ae:d6:64 # ml110g7-2
+
+ML110G7_2="e4:11:5b:ae:d6:64"
+wakeonlan ${ML110G7_2:?}
 RC=$?
 echo "wol:${RC:?}"
+
 [ -x /usr/sbin/ufw ] && sudo ufw -f enable
 [ -x /usr/sbin/ufw ] && sudo ufw status
 
