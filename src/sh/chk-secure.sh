f46d7b3e1f02f98a091d7279e17100476cf63b88
Tatsuo Indou <indou.tsystem@gmail.com>
2018-04-09 16:39:07 +0900

    update chk-secure.sh

--- src/sh/chk-secure.sh
+++ src/sh/chk-secure.sh
@@ -8,13 +8,13 @@ NAME=${0##*/}
 SECURELOG_PATH="/var/log/secure*"
 
 echo "接続成功"
-sudo grep "Accepted" /var/log/secure* | awk '{print $9, $11;}' | sort | uniq -c | more
+sudo grep "Accepted" /var/log/secure* | awk '{print $9, $11;}' | sort -n | uniq -c | sort -n | more
 
 echo -n "OK?:"
 read OK
 
 echo "接続失敗"
-sudo grep -E "Failed|Invalid" /var/log/secure* | awk '{print $10;}' | sort | uniq -c | sort | more
+sudo grep -E "Failed|Invalid" /var/log/secure* | awk '{print $10;}' | sort | uniq -c | sort -n | more
 
 echo "done"
 
