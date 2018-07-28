c7212748ba987e623029ac2aed0d3c1be1caba70
Tatsuo Indou <indou.tsystem@gmail.com>
2018-07-28 15:21:34 +0900

    update get-db-charset.sh

--- src/sh/get-db-charset.sh
+++ src/sh/get-db-charset.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-sqlplus / as sysdba <<EOT
+sqlplus -S / as sysdba <<EOT
 
 whenever sqlerror exit failure
 
-SELECT PARAMETER, VALUE FROM NLS_DATABASE_PARAMETERS WHERE PARAMETER = 'NLS_CHARACTERS;
+column parameter format a20
+column value     format a20
+
+SELECT PARAMETER, VALUE FROM NLS_DATABASE_PARAMETERS WHERE PARAMETER = 'NLS_CHARACTERSET';
 
 exit 0
 EOT
