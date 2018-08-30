338477bf6f33547ddada09f71380992c06a5fbba
Tatsuo Indou <indou.tsystem@gmail.com>
2018-08-30 17:33:54 +0900

    update chk-all.sh

--- src/sh/chk-all.sh
+++ src/sh/chk-all.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# ping
+# -s: データのバイト数
+# -c: パケット数
+# -W: レスポンスの待ち時間(秒)
+
 NAMES="ml110g7 ml110g7-2 t3600 vip rpi-bp rpi2 rpi-b macbook macmini2010 rpi3-1 rpi3-2 stormZ170 r60e $*"
 
 for NAME in ${NAMES:?}
 do
-  ( ping -c 1 $NAME > /dev/null; RC=$?; echo "${RC:?}:ping $NAME" > /tmp/${0##*/}.${NAME:?} ) &
+  ( ping -W 1 -s 1 -c 1 $NAME > /dev/null; RC=$?; echo "${RC:?}:ping $NAME" > /tmp/${0##*/}.${NAME:?} ) &
 done
 wait
 for NAME in /tmp/${0##*/}.*
