b0552d6db6f1f8b7447fc4c0de34105d519aff46
Tatsuo Indou <indou.tsystem@gmail.com>
2019-01-10 21:48:23 +0900

    update mail-volumioIP.sh

--- src/sh/mail-volumioIP.sh
+++ src/sh/mail-volumioIP.sh
@@ -2,24 +2,24 @@
 #
 SCRIPT=${0##*/}
 TMP=/tmp/${SCRIPT:?}.$$.tmp
-MAILTO="indou.tsystem@docomo.ne.jp,toshikoyumechan@yahoo.ne.jp"
+MAILTO="indou.tsystem@gmail.com,toshikoyumechan@yahoo.ne.jp"
 
 TERM() {
   rm -f /tmp/${0##*/}.$$.tmp
 }
 
-trap 'TERM' 0
+#trap 'TERM' 0
 
 DATE=`date '+%Y%m%d %H%M%S'`
 
-ping -s1 -c1 -W1 volumio.local > ${TMP:?} # １アドレス毎、256パラ
+ping -s1 -c1 -W1 volumio.local | grep "ttl" >  ${TMP:?} # １アドレス毎、256パラ
 RC=$?
 if [ "${RC:?}" -ne "0" ]; then
   echo "${SCRIPT:?}: ping fail. ${RC:?}" | mail -s "${DATE:?} ${SCRIPT:?}: ping fail." ${MAILTO:?}
   exit 9
 fi
 
-ADDRESS=$(grep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' ${TMP:?})
+ADDRESS=$(egrep -o 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' ${TMP:?} | sed 's/from //')
 RC=$?
 if [ "${RC:?}" -ne "0" ]; then
   echo "${SCRIPT:?}: grep fail. ${RC:?}" | mail -s "${DATE:?} ${SCRIPT:?}: ping fail." ${MAILTO:?}
@@ -27,14 +27,8 @@ if [ "${RC:?}" -ne "0" ]; then
 fi
 
 cat <<EOT | mail -s "${DATE:?} Volumio's IP" ${MAILTO:?}
-<!doctype html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-</head>
-<body>
-<p>${DATE:?}</p>
-<a href="https://${ADDRESS:?}/">volumio</a>
+ ** volumio **
+${ADDRESS:?}
 </body>
 EOT
 
