570eb0a9342aa0f74e719119549e3e11f6b611b2
Tatsuo Indou <indou.tsystem@gmail.com>
2019-01-19 16:38:06 +0900

    update failover.sh

---
+++ src/sh/failover.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# サーバ/インフラを支える技術 P.9
+#
+#
+SCRIPT=`basename $0`
+VIP="192.168.0.254"  # 仮想IP
+DEV="eth0"           # NIC名
+LOG=/tmp/${0##*/}.log
+
+main() {
+  while healthcheck
+  do
+    echo "${SCRIPT:?}: $(date '+%Y%m%d%H%M%S'): ${VIP:?} health ok!" >> ${LOG:?} 
+    sleep 1
+  done
+  echo "fail over!"
+  ip_takeover
+}
+
+# $VIPの生死監視
+# 0 :生
+# 0<:死
+healthcheck() {
+  # c1 1 : 一回
+  # -w 1 : タイムアウト1sec
+  #ping -c 1 -w 1 $VIP >/dev/null
+  
+  curl $VIP > /dev/null 2>&1
+  return $?
+}
+
+# $VIPと、$DEVのMACアドレスをGratuitous ARPで、ブロードキャスト
+ip_takeover() {
+  # $DEVのMACを取得
+  MAC=`ip link show $DEV | egrep -o '([0-9a-f]{2}:){5}[0-9a-f]{2}' | head -n1 | tr -d :`
+  # $DEVに、$VIPをlabel付きで追加する
+  ip addr add $VIP/24 dev $DEV label $DEV:V1
+  ip addr show
+  ifconfig
+  # $VIPと$MAXをブロードキャスト
+  send_arp ${VIP:?} ${MAC:?} 255.255.255.255 ffffffffffff
+}
+
+main
