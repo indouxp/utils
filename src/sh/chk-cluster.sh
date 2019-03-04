eb58505636dbf3abab2fd759baa6d88bd944f700
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2019-03-04 15:59:14 +0900

    update chk-cluster.sh

--- src/sh/chk-cluster.sh
+++ src/sh/chk-cluster.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-
+###############################################################################
+#
+# lxc上の$NODE1と$NODE2のeth0のIPと、VIPを取得し、
+#
+#
+###############################################################################
 set -e
 
+# lxc上の$NODE1と$NODE2のeth0のIPと、VIPを取得
 for host in c7-1 c7-2
 do
 	NUM=$(lxc ls ${host:?} | grep eth0 | wc -l)
@@ -10,11 +16,17 @@ do
 	fi
 done
 
+# 二つのIP(VIP)を持つアドレスを取得。後ろ側のアドレスが変数ipに代入される
 for ip in $(lxc ls ${host:?} | grep -o "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]")
 do
 	:
 done
 
+後ろ側が、VIPなので、hタグの内容を取得する
 HOST=$(curl -s ${ip:?} | grep -o "<h1>[^<]*</h1>" | sed 's/<h1>//; s/<\/h1>//')
 
-echo $HOST active
+if [ "${HOST}" == "" ]; then
+  echo no active
+else
+  echo $HOST active
+fi
