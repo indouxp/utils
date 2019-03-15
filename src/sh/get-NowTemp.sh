633e91f803f34152ea5a0fb5541153b40746f175
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2019-03-15 11:58:11 +0900

    update get-NowTemp.sh

--- src/sh/get-NowTemp.sh
+++ src/sh/get-NowTemp.sh
@@ -6,16 +6,27 @@
 #
 ###############################################################################
 NAME=${0##*/}
-OUT=$(echo ${NAME:?} | sed 's/\.sh/.out/')
-ORG=$(echo ${NAME:?} | sed 's/\.sh/.org/')
+OUT=/tmp/${NAME:?}.out.$$
+ORG=/tmp/${NAME:?}.org.$$
 PLACE="八王子"
+PLACE=$1
 
-URL="https://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_201903140900.csv"
+###############################################################################
+TERM(){
+  for SUFFIX in out org
+  do
+    mv /tmp/${0##*/}.${SUFFIX:?}.$$ /tmp/${0##*/}.${SUFFIX:?}
+  done
+}
+trap 'TERM' 0
+
+###############################################################################
 # https://www.data.jma.go.jp/obd/stats/data/mdrr/docs/csv_dl_format_mxtem.html
 MAXTEMP="https://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv"
 # https://www.data.jma.go.jp/obd/stats/data/mdrr/docs/csv_dl_format_mntem.html
 MINTEMP="https://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mntemsadext00_rct.csv"
 
+###############################################################################
 if [ ! -x $(which nkf) ]; then
   echo "${NAME:?}: nkf not found." 1>&2
   exit 9
@@ -25,12 +36,13 @@ if [ ! -x $(which gawk) ]; then
   exit 9
 fi
 
+###############################################################################
 for TEMP in ${MAXTEMP:?}@MAX ${MINTEMP:?}@MIN
 do
   WHICH=$(echo ${TEMP:?} | sed 's/[^@]*@//')
   URL=$(echo ${TEMP:?} | sed 's/@[^@]*//')
   if wget -O ${OUT:?} ${URL:?} 2>/dev/null; then
-    nkf -w ${OUT:?} |
+    nkf -w -Lu -d ${OUT:?} |
     grep ${PLACE:?} |
     tee  ${ORG:?}   |
     gawk -v FPAT='([^,]+)|(\"[^\"]+\")' \
@@ -41,4 +53,6 @@ do
     #cat ${ORG:?}
   fi
 done
+
+###############################################################################
 exit 0
