dd58a0a693b6a5b588137daf1e3b32859ce42be3
Tatsuo Indou <indou.tsystem@gmail.com>
2019-06-21 23:03:32 +0900

    add run-rdesktop.sh

---
+++ src/sh/run-rdesktop.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+################################################################################
+#
+# rdesktop.shで、接続
+#
+################################################################################
+NAME=${0##*/}
+DIR=${0%/*}
+if [ "$#" -eq "0" ]; then
+  cat <<EOT
+Usage
+\$ ${NAME:?} HOST USER 
+EOT
+  exit 0
+fi
+
+HOST=$1
+USER=$2
+
+if [ ! -x ${DIR:?}/rdesktop.sh ]; then
+  echo "${NAME:?}: ${DIR:?}/rdesktop.sh not exist" 1>&2
+  exit 9
+fi
+
+while true
+do
+  nmap -P0 ${HOST:?} | grep 3389
+  RC=$?
+  if [ "${RC:?}" -eq "0" ]; then
+    break
+  fi
+done
+
+${DIR:?}/rdesktop.sh ${HOST:?} ${USER:?}
+exit $?
+
