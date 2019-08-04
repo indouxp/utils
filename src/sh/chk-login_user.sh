331b76506320acf1093c769acddd915f2e1edf1a
Tatsuo Indou <indou.tsystem@gmail.com>
2019-08-04 15:03:48 +0900

    update chk-login_user.sh

--- src/sh/chk-login_user.sh
+++ src/sh/chk-login_user.sh
@@ -13,13 +13,13 @@ DF='+%Y/%m/%d %H:%M:%S'
 #
 # 最終ログイン履歴からindou以外を抽出
 #
-last | grep -v indou | grep -v "wtmp" > ${TMP:?}
+last | grep -v indou | grep -v "wtmp" | grep -v "reboot" > ${TMP:?}
 RC=$?
 if [ "${RC:?}" -ne "0" ]; then
-  echo "${NAME:?}:ERROR 010" 1>&2
+  echo "${NAME:?}: $(date $DF) ERROR 010" >> ${LOG:?}
   exit ${RC:?}
 fi
-echo "${NAME:?}: $(date $DF) last, grep OK" > ${LOG:?}
+echo "${NAME:?}: $(date $DF) last, grep OK" >> ${LOG:?}
 
 #
 # ログイン履歴数カウント
@@ -27,24 +27,24 @@ echo "${NAME:?}: $(date $DF) last, grep OK" > ${LOG:?}
 NUM=$(awk '{if(NF>0){print;}}' ${TMP:?} | wc -l)
 RC=$?
 if [ "${RC:?}" -ne "0" ]; then
-  echo "${NAME:?}:ERROR 020" 1>&2
+  echo "${NAME:?}: $(date $DF) ERROR 020" >> ${LOG:?}
   exit ${RC:?}
 fi
-echo "${NAME:?}: $(date $DF) awk, wc OK" > ${LOG:?}
+echo "${NAME:?}: $(date $DF) awk, wc OK" >> ${LOG:?}
 
 #
 # ログイン履歴がある場合は、エラー
 #
 if [ "${NUM:?}" -ne "0" ]; then
-  echo "${NAME:?}:ERROR 030" 1>&2
+  echo "${NAME:?}: $(date $DF) ERROR 030" >> ${LOG:?}
   mv ${TMP:?} /home/indou
   if [ ! -e /tmp/${NAME:?}.stop ]; then
-    logger "${NAME:?} shutdown at `date '+%Y/%m/%d %H:%M:%S'`"
-    shutdown -h now
+    logger "${NAME:?} shutdown at $(date $DF)"
+    #shutdown -h now
   fi
   exit ${NUM:?}
 fi
-echo "${NAME:?}: $(date $DF) others no login" > ${LOG:?}
+echo "${NAME:?}: $(date $DF) others no login" >> ${LOG:?}
 
 #
 # 正常終了
