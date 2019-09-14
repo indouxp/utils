aaa9373fe7b9098a7cdd1febd0a8a86044e43391
Tatsuo Indou <indou.tsystem@gmail.com>
2019-09-15 00:29:06 +0900

    add  volumio4.sh

---
+++ src/sh/volumio4.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+###############################################################################
+#
+# volumioのリモート実行
+#
+###############################################################################
+NAME=${0##*/}
+USER=volumio  # cronからだと、未定義
+LOG_DIR=/home/${USER:?}/log
+LOG_PATH=$LOG_DIR/${NAME:?}.log
+PATH=/bin:/usr/bin:/usr/local/bin
+
+date                            >> ${LOG_PATH:?}
+if [ ! -x $(which curl) ]; then
+  echo "${NAME:?}: curl not found." | tee -a ${LOG_PATH:?}
+fi
+if [ ! -x $(which jq) ]; then
+  echo "${NAME:?}: jq not found." | tee -a ${LOG_PATH:?}
+fi
+
+PLAYLIST=$1
+VOL=$2
+echo "PLAYLIST: ${PLAYLIST:?}"  >> ${LOG_PATH:?}
+echo "VOLUME  : ${VOL:?}"       >> ${LOG_PATH:?}
+
+if curl "http://localhost:3000/api/v1/getState" \
+  >> ${LOG_PATH:?} 2>&1                                     |\
+  jq '.status" | grep "play"
+then
+  exit 0
+fi
+
+curl "http://localhost:3000/api/v1/commands?cmd=playplaylist&name=${PLAYLIST:?}" \
+  >> ${LOG_PATH:?} 2>&1                                      |\
+  jq '.response' >> ${LOG_PATH:?}
+
+curl "http://localhost:3000/api/v1/commands?cmd=volume&volume=${VOL:?}" \
+  >> ${LOG_PATH:?} 2>&1                                      |\
+  jq '.response' >> ${LOG_PATH:?}
