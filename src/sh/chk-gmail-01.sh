ee05a1eb52ea28126063e3e72774a56dcfb32c1a
Tatsuo Indou <indou.tsystem@gmail.com>
2019-11-27 22:52:21 +0900

    Update chk-gmail-01.sh

--- src/sh/chk-gmail-01.sh
+++ src/sh/chk-gmail-01.sh
@@ -18,3 +18,25 @@ expect -c "
   expect \"+OK Farewell.\"
   exit 0
 " > receive.log
+
+RECEIVE_COUNT=$(grep +OK receive.log | tail -n2 | head -n1 |cut -d " " -f 2)
+echo $RECEIVE_COUNT
+
+rm -f messages.log
+#for i in $(seq ${RECEIVE_COUNT:?} -1 1)
+#do
+  expect -c "
+    set timeout 30
+    spawn openssl s_client -connect ${POP_SERVER:?}:995
+    expect \"+OK Gpop ready\"
+    send \"user ${USER_ID:?}\n\"
+    expect \"+OK send PASS\"
+    send \"pass ${PASSWORD:?}\n\"
+    expect \"+OK Welcome.\"
+    send \"retr ${RECEIVE_COUNT:?}\n\"
+    expect \".\"
+    send \"quit\n\"
+    expect \"+OK Farewell.\"
+    exit 0
+  " | nkf -w > messages.log
+#done
