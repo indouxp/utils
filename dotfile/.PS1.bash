021cea919d7993c3f411d5453615a38bca32e095
Tatsuo Indou <indou.tsystem@gmail.com>
2020-06-18 11:52:17 +0900

    update .PS1.bash

--- dotfile/.PS1.bash
+++ dotfile/.PS1.bash
@@ -1,4 +1,5 @@
 # http://d.hatena.ne.jp/zariganitosh/20150224/escape_sequence
+# https://qiita.com/PruneMazui/items/8a023347772620025ad6
 RED="\[\e[31m\]"
 GREEN="\[\e[32m\]"
 YELLOW="\[\e[33m\]"
@@ -6,13 +7,13 @@ BLUE="\[\e[34m\]"
 MAGENTA="\[\e[35m\]"
 CYAN="\[\e[36m\]"
 WHITE="\[\e[37m\]"
-CLEAR="\[\e[0m\]"
+CLEAR="\[\e[0m\]"                         # 指定をリセットし未指定状態に戻す
+BLINK="\[\e[5m\]"                         # ブリンク
+BOLD="\[\e[1m\]"                          # 太字
+TURN="\[\e[7m\]"                          # 文字色と背景色の反転
+UNDER="\[\e[4m\]"                         # アンダーライン
 if id | grep "=0[^0-9]" > /dev/null; then # uid=0
-  #PS1="\$? \[\e[36m\e[41m\]\D{%y%m%d-%H%M%S} \u@\h:\W:\$\[\e[0m\] "
-  #PS1="\$? \[\e[31m\e[40m\]\D{%y%m%d-%H%M%S} \u@\h:\W:\$\[\e[0m\] "
-  PS1="${RED:?}\$?${CLEAR:?} \D{%y%m%d-%H%M%S} ${RED:?}\u${CLEAR:?}@\h:${YELLOW:?}\W${CLEAR:?}:\n# "
+  PS1="${RED:?}\$?${CLEAR:?} \D{%y%m%d-%H%M%S} ${RED:?}\u${CLEAR:?}@${UNDER:?}\h${CLEAR:?}:${YELLOW:?}\W${CLEAR:?}:\n# "
 else
-  #PS1="\$? \[\e[37m\e[44m\]\D{%y%m%d-%H%M%S} \u@\h:\W:\$\[\e[0m\] "
-  #PS1="\$? \[\e[36m\e[40m\]\D{%y%m%d-%H%M%S} \u@\h:\W:\$\[\e[0m\] "
-  PS1="${RED:?}\$?${CLEAR:?} \D{%y%m%d-%H%M%S} ${GREEN:?}\u${CLEAR:?}@\h:${YELLOW:?}\W${CLEAR:?}:\n\$ "
+  PS1="${RED:?}\$?${CLEAR:?} \D{%y%m%d-%H%M%S} ${GREEN:?}\u${CLEAR:?}@${UNDER:?}\h${CLEAR:?}:${YELLOW:?}\W${CLEAR:?}:\n\$ "
 fi
